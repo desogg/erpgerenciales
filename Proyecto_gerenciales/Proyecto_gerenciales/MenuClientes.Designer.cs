de025b8181b32c76b49b702cd14d5f73bd368ca5
Diego Osorio <3476164370101@ingenieria.usac.edu.gt>
2019-12-22 10:57:02 -0600

    Creación de la funcionalidad Editar Clientes
    Se creo la funcionalidad Editar Clientes, junto con su respectivo botón en el menú clientes para acceder a él.

--- Proyecto_gerenciales/Proyecto_gerenciales/MenuClientes.Designer.cs
+++ Proyecto_gerenciales/Proyecto_gerenciales/MenuClientes.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -45,19 +46,31 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(99, 375);
+            this.button2.Location = new System.Drawing.Point(99, 278);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(159, 78);
             this.button2.TabIndex = 1;
             this.button2.Text = "Editar Cliente";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(99, 410);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 78);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Regresar al menú principal";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // MenuClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(407, 589);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -72,5 +85,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
